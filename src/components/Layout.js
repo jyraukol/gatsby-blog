b9c4d675122585661e19d5e4a552a74eaa60b51c
Jyri Raukola <jr@jyrirauko.la>
2019-02-05 20:46:57 +0200

    Add a background image from Subtle Patterns. Rounded corners and box-shadows

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,74 +1,34 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { rhythm } from '../utils/typography'
 
-import { rhythm, scale } from '../utils/typography'
+import Header from "./Header"
+import "./layout.css"
 
 class Layout extends React.Component {
   render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
+    const {children, title} = this.props
 
-    //if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    /*} else {
-      header = (
-        <h3
-          style={{
-            fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0,
-            marginBottom: rhythm(-1),
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }*/
     return (
       <div
         style={{
+          marginTop: `1em`,
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          backgroundColor: `#f8f8ff`,
+          borderRadius: `0 0 50% 0`,    
+          boxShadow: `5px 10px 5px slategrey`,
+          maxWidth: rhythm(34),
+          padding: `${rhythm(1.5)} ${rhythm(3)}`,
         }}
       >
-        {header}
+        <Header title={title}/>
         {children}
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-</div>
+      </div>
     )
   }
 }
